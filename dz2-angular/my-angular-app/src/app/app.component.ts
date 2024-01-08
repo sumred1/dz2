d3cbf5a921fc226f771ee8055ecd11016a60ae2d
sumred1 <dlagun47@gmail.com>
2024-01-08 23:30:56 +0200

    dz2
    dz2

---
+++ dz2-angular/my-angular-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-angular-app';
+  InfoAboutWork = `5 years of work at a factory and 2 years of work as a programmer`;
+
+}
---
+++ dz2-angular/my-angular-app/src/app/serfiticats/serfiticats.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-serfiticats',
+  templateUrl: './serfiticats.component.html',
+  styleUrls: ['./serfiticats.component.scss']
+})
+export class SerfiticatsComponent {
+
+}
